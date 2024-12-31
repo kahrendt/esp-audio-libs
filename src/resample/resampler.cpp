5b7cc065fdc7de0c7256c51e6f9a769135403d7a
Kevin Ahrendt <kevin.ahrendt@nabucasa.com>
2024-12-31 15:28:33 -0500

    copy directly to output buffer if resampling isn't necessary

--- src/resample/resampler.cpp
+++ src/resample/resampler.cpp
@@ -96,8 +96,14 @@ ResamplerResults Resampler::resample(const uint8_t *input_buffer, uint8_t *outpu
 
   unsigned int frames_to_process = std::min(input_frames_available, necessary_frames);
 
-  quantized_to_float(input_buffer, this->float_input_buffer_, frames_to_process * this->channels_, this->input_bits_,
-                     gain_db);
+  if (this->requires_resampling_) {
+    quantized_to_float(input_buffer, this->float_input_buffer_, frames_to_process * this->channels_, this->input_bits_,
+                       gain_db);
+  } else {
+    // Just converting the bits per sample
+    quantized_to_float(input_buffer, this->float_output_buffer_, frames_to_process * this->channels_, this->input_bits_,
+                       gain_db);
+  }
 
   size_t frames_used = frames_to_process;
   size_t frames_generated = frames_to_process;
@@ -126,8 +132,8 @@ ResamplerResults Resampler::resample(const uint8_t *input_buffer, uint8_t *outpu
     }
   }
 
-  uint32_t clipped_samples =
-      float_to_quantized(this->float_output_buffer_, output_buffer, frames_generated*this->channels_, this->output_bits_);
+  uint32_t clipped_samples = float_to_quantized(this->float_output_buffer_, output_buffer,
+                                                frames_generated * this->channels_, this->output_bits_);
 
   ResamplerResults results = {.frames_used = frames_used,
                               .frames_generated = frames_generated,
